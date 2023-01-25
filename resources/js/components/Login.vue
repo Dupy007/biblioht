25962d4095fd4bbb1679345fcf901d75cda64a69
dupy007 <duperrierjosephfils@gmail.com>
2023-01-25 22:16:37 +0100

    pwa

---
+++ resources/js/components/Login.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Login</div>
+
+                <div class="card-body">
+                    <form @submit="checkForm">
+                        <div class="row">
+                            <p v-if="errors.length" class="bg-danger">
+                                <b>Please correct the following error(s):</b>
+                                <ul>
+                                <li v-for="error in errors">{{ error }}</li>
+                                </ul>
+                            </p>
+                            <div class="row mb-3">
+                                <label for="email" class="col-md-4 col-form-label text-md-end">Email Address</label>
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control" name="email" required autocomplete="email" autofocus  v-model="user.email">
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label for="password" class="col-md-4 col-form-label text-md-end">Password</label>
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control" name="password" required  v-model="user.password" >
+                                </div>
+                            </div>
+                            <div class="row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Login
+                                    </button>
+                                        <a class="btn btn-link">
+                                            Forgot Your Password?
+                                        </a>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default{
+    name:"login-form",
+    data(){
+        return{
+            user:{
+                email:"",
+                password:"",
+            },
+            errors:[],
+        }
+    },
+    methods:{
+        async login(){
+            await axios.get('/sanctum/csrf-cookie').then(response => {
+                axios.post('/login', this.user).then(response=>{
+                    this.$router.push({name:"home"})
+                }).catch(error=>{
+                    var theeerrors= [];
+                    theeerrors = error.response.data.errors;
+                    for (const key in theeerrors) {
+                        if (Object.hasOwnProperty.call(theeerrors, key)) {
+                            const element = theeerrors[key];
+                            this.errors.push(element[0]);
+                        }
+                    }
+                    console.log('error =>' ,error);
+                })
+            });
+        },
+        checkForm:function(e) {
+            if(this.user.password && this.user.email  ) this.login();
+            this.errors = [];
+            if(!this.user.password) this.errors.push("Password required.");
+            if(!this.user.email) this.errors.push("Email required.");
+            e.preventDefault();
+        }
+    }
+}
+</script>
---
+++ resources/js/components/Register.vue
@@ -0,0 +1,103 @@
+<template>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Add User</h4>
+                </div>
+                <div class="card-body">
+                    <!-- <form @submit.prevent="create" @submit="checkForm"> -->
+                    <form @submit="checkForm">
+                        <div class="row">
+                            <p v-if="errors.length" class="bg-danger">
+                                <b>Please correct the following error(s):</b>
+                                <ul>
+                                <li v-for="error in errors">{{ error }}</li>
+                                </ul>
+                            </p>
+                            <div class="col-12 mb-2">
+                                <div class="form-group">
+                                    <label>Name</label>
+                                    <input type="text" class="form-control" v-model="user.name">
+                                </div>
+                            </div>
+                            <div class="col-12 mb-2">
+                                <div class="form-group">
+                                    <label>Mobile</label>
+                                    <input type="text" class="form-control" v-model="user.mobile_no">
+                                </div>
+                            </div>
+                            <div class="col-12 mb-2">
+                                <div class="form-group">
+                                    <label>Email</label>
+                                    <input type="mail" class="form-control" v-model="user.email">
+                                </div>
+                            </div>
+                            <div class="col-12 mb-2">
+                                <div class="form-group">
+                                    <label>Type</label>
+                                    <input type="text" class="form-control " v-model="user.type_account">
+                                </div>
+                            </div>
+                            <div class="col-12 mb-2">
+                                <div class="form-group">
+                                    <label>Code parrain</label>
+                                    <input type="text" class="form-control " v-model="user.parrain">
+                                </div>
+                            </div>
+                            <div class="col-12 mb-2">
+                                <button type="submit" class="btn btn-primary">Save</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name:"add-user",
+    data(){
+        return{
+            user:{
+                name:"",
+                mobile_no:"",
+                email:"",
+                code:"",
+                parrain:"",
+                type_account:"user",
+                password:"PLFMotDePasse",
+                password_confirmation:"PLFMotDePasse",
+            },
+            errors:[],
+        }
+    },
+    methods:{
+        async create(){
+            await axios.post('/api/user', this.user).then(response=>{
+                this.$router.push({name:"userList"})
+            }).catch(error=>{
+                var theeerrors= [];
+                theeerrors = error.response.data.errors;
+                for (const key in theeerrors) {
+                    if (Object.hasOwnProperty.call(theeerrors, key)) {
+                        const element = theeerrors[key];
+                        this.errors.push(element[0]);
+                    }
+                }
+                console.log('error =>' ,error);
+            })
+        },
+        checkForm:function(e) {
+            if(this.user.name && this.user.email  ) this.create();
+            this.errors = [];
+            if(!this.user.name) this.errors.push("Name required.");
+            if(!this.user.email) this.errors.push("Email required.");
+            if(!this.user.mobile_no) this.errors.push("Mobile required.");
+            e.preventDefault();
+        }
+    }
+}
+</script>
